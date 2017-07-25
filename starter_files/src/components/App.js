ff39ba942f429c30e18cfde6064f7c353b812225
newline-power-tools <doug@theironyard.com>
2017-07-25 02:09:50 +0000

    570e50d0-starter_files.zip

---
+++ starter_files/src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+// Import BaseLayout, Appetizers, Entrees, Desserts
+
+
+
+class App extends Component {
+// Set initial state for appetizers, entrees, and desserts.
+// All should be set to empty arrays.
+
+// Lifecycle method
+// Fetch from http://tiny-lasagna-server.herokuapp.com/collections/reactthaimenu.
+// The response should return an object with appetizers, entres, and desserts.
+// Set these to state.
+// YOUR CODE HERE>
+
+  render() {
+    // Your render should consist of the BaseLayout with the following children componenets: Appetizers, Entres, and Dessert.
+    // Each component needs to receive state via props.
+    return (
+      {/*
+        BaseLayout and nested components
+        */}
+    );
+  }
+}
+
+export default App;
